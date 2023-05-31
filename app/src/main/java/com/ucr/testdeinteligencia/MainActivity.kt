c57f2057bf7cf1a19c31782853246ced3aad16a0
buraiang99 <bryangsu10@gmail.com>
2023-05-31 16:33:35 -0600

    Initial commit

---
+++ app/src/main/java/com/ucr/testdeinteligencia/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ucr.testdeinteligencia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
